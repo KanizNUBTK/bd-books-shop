4841e0ffffc471823d8c59980fd299afc18d5ea1
KanizNUBTK <kaniz.m.mim@gmail.com>
2021-10-29 00:22:08 +0600

    auth added

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+console.log(process.env);
+const firebaseConfig = {
+    apiKey: "AIzaSyC6NkStBgN2LmsmMMcOPQyJty64VUWZn_I",
+    authDomain: "bd-book-delivery-shop.firebaseapp.com",
+    projectId: "bd-book-delivery-shop",
+    storageBucket: "bd-book-delivery-shop.appspot.com",
+    messagingSenderId: "67243830787",
+    appId: "1:67243830787:web:69adda950482b5d5afe836"
+  };
+
+export default firebaseConfig;
